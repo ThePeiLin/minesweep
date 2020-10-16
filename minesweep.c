40f90859044e1505cd1c4c87358b628a333c7776
ThePeiLin <47449134+ThePeiLin@users.noreply.github.com>
2020-10-16 14:01:58 +0800

    Add files via upload
    fix bug

--- minesweep.c
+++ minesweep.c
@@ -39,11 +39,11 @@ int main(int argc,char **argv)
 				case 'B':line=18;row=18;bombNumber=18;break;
 				case 'd':
 				case 'D':
-						printf("input line and row\n");
-						scanf("%d %d",&line,&row);
-						printf("How many landmines you want?\n");
-						scanf("%d",&bombNumber);
-						break;
+						 printf("input line and row\n");
+						 scanf("%d %d",&line,&row);
+						 printf("How many landmines you want?\n");
+						 scanf("%d",&bombNumber);
+						 break;
 				case '?':
 				case 'h':
 				case 'H':printf("-A or -H\n");exit(0);break;
@@ -112,18 +112,18 @@ void createMap()
 		for(xc=1;xc<=row;xc++)
 		{
 			(pb[ARRY(xc,yc)].back!=9)?
-			({
-				pb[ARRY(xc,yc)].back+=(pb[ARRY(xc-1,yc-1)].back==9)?1:0;
-				pb[ARRY(xc,yc)].back+=(pb[ARRY(xc,yc-1)].back==9)?1:0;
-				pb[ARRY(xc,yc)].back+=(pb[ARRY(xc+1,yc-1)].back==9)?1:0;
-				pb[ARRY(xc,yc)].back+=(pb[ARRY(xc-1,yc)].back==9)?1:0;
-				pb[ARRY(xc,yc)].back+=(pb[ARRY(xc+1,yc)].back==9)?1:0;
-				pb[ARRY(xc,yc)].back+=(pb[ARRY(xc-1,yc+1)].back==9)?1:0;
-				pb[ARRY(xc,yc)].back+=(pb[ARRY(xc,yc+1)].back==9)?1:0;
-				pb[ARRY(xc,yc)].back+=(pb[ARRY(xc+1,yc+1)].back==9)?1:0;
-			}):0;
+				({
+				 pb[ARRY(xc,yc)].back+=(pb[ARRY(xc-1,yc-1)].back==9)?1:0;
+				 pb[ARRY(xc,yc)].back+=(pb[ARRY(xc,yc-1)].back==9)?1:0;
+				 pb[ARRY(xc,yc)].back+=(pb[ARRY(xc+1,yc-1)].back==9)?1:0;
+				 pb[ARRY(xc,yc)].back+=(pb[ARRY(xc-1,yc)].back==9)?1:0;
+				 pb[ARRY(xc,yc)].back+=(pb[ARRY(xc+1,yc)].back==9)?1:0;
+				 pb[ARRY(xc,yc)].back+=(pb[ARRY(xc-1,yc+1)].back==9)?1:0;
+				 pb[ARRY(xc,yc)].back+=(pb[ARRY(xc,yc+1)].back==9)?1:0;
+				 pb[ARRY(xc,yc)].back+=(pb[ARRY(xc+1,yc+1)].back==9)?1:0;
+				 }):0;
 		}
-	
+
 	}
 	xc=0;yc=0;
 	realBombNumber=bombNumber;
@@ -164,18 +164,18 @@ void print()
 			switch(pb[ARRY(xp,yp)].front)
 			{
 				case 0:switch(pb[ARRY(xp,yp)].back)
-				{
-						case 0:printf("\033[0m\033[%dm.",light);break;
-						case 1:printf("\033[0m\033[%dm1",light);break;
-						case 2:printf("\033[0m\033[%dm2",light);break;
-						case 3:printf("\033[0m\033[%dm3",light);break;
-						case 4:printf("\033[0m\033[%dm4",light);break;
-						case 5:printf("\033[0m\033[%dm5",light);break;
-						case 6:printf("\033[0m\033[%dm6",light);break;
-						case 7:printf("\033[0m\033[%dm7",light);break;
-						case 8:printf("\033[0m\033[%dm8",light);break;
-						case 9:printf("\033[0m\033[%dm*",light);break;
-				}break;
+					   {
+						   case 0:printf("\033[0m\033[%dm.",light);break;
+						   case 1:printf("\033[0m\033[%dm1",light);break;
+						   case 2:printf("\033[0m\033[%dm2",light);break;
+						   case 3:printf("\033[0m\033[%dm3",light);break;
+						   case 4:printf("\033[0m\033[%dm4",light);break;
+						   case 5:printf("\033[0m\033[%dm5",light);break;
+						   case 6:printf("\033[0m\033[%dm6",light);break;
+						   case 7:printf("\033[0m\033[%dm7",light);break;
+						   case 8:printf("\033[0m\033[%dm8",light);break;
+						   case 9:printf("\033[0m\033[%dm*",light);break;
+					   }break;
 				case 1:printf("\033[0m\033[%dm=",light);break;
 				case 2:printf("\033[31m\033[%dm!",light);break;
 				case 3:printf("\033[0m\033[%dm?",light);break;
@@ -224,48 +224,48 @@ void openBlock()
 	{
 		realBombNumber+=(pb[ARRY(x,y)].front==2)?1:0;
 		(pb[ARRY(x,y)].back==0)?
-		({
-			int tier;
-			int xt,yt;
-			pb[ARRY(x,y)].front=0;
-			for(tier=1;({
-					int canOpenBlock=0;
-					for(yt=tier;yt>(-tier-1)&&canOpenBlock==0;yt--)
-						for(xt=tier;xt>(-tier-1)&&canOpenBlock==0;xt-=((yt==tier)||(yt==(-tier)))?1:(tier*2))
-							canOpenBlock=(((y-yt>=1)&&(y-yt<=line)&&(x-xt>=1)&&(x-xt<=row))&&
-								(((pb[ARRY(x-xt-1,y-yt)].front==1)&&(pb[ARRY(x-xt-1,y-yt)].back==0))
-								||((pb[ARRY(x-xt+1,y-yt)].front==1)&&(pb[ARRY(x-xt+1,y-yt)].back==0))
-								||((pb[ARRY(x-xt,y-yt-1)].front==1)&&(pb[ARRY(x-xt,y-yt-1)].back==0))
-								||((pb[ARRY(x-xt,y-yt+1)].front==1)&&(pb[ARRY(x-xt,y-yt+1)].back==0)))
-								&&((pb[ARRY(x-xt,y-yt)].front==1)&&(pb[ARRY(x-xt,y-yt)].back!=9)))?
-								1
-								:0;
-					canOpenBlock;})
-					;tier+=(
-			{
-				int haveUnopenedBlock=1;
-				for(yt=tier;yt>(-tier-1)&&haveUnopenedBlock==0;yt--)
-					for(xt=tier;xt>(-tier-1)&&haveUnopenedBlock==0;xt-=((yt==tier)||(yt==(-tier)))?1:(tier*2))
-						haveUnopenedBlock=(((y-yt>=1)&&(y-yt<=line)&&(x-xt>=1)&&(x-xt<=row))&&
-							(((pb[ARRY(x-xt-1,y-yt)].front!=4)&&(pb[ARRY(x-xt-1,y-yt)].back==0))
-							||((pb[ARRY(x-xt+1,y-yt)].front!=4)&&(pb[ARRY(x-xt+1,y-yt)].back==0))
-							||((pb[ARRY(x-xt,y-yt-1)].front!=4)&&(pb[ARRY(x-xt,y-yt-1)].back==0))
-							||((pb[ARRY(x-xt,y-yt+1)].front!=4)&&(pb[ARRY(x-xt,y-yt+1)].back==0)))
-							&&((pb[ARRY(x-xt,y-yt)].front==1)&&(pb[ARRY(x-xt,y-yt)].back!=9)))?
-					1
-					:0;
-				haveUnopenedBlock;
-			}))
-				for(yt=tier;yt>(-tier-1);yt--)
-					for(xt=tier;xt>(-tier-1);xt-=((yt==tier)||(yt==(-tier)))?1:(tier*2))
-						pb[ARRY(x-xt,y-yt)].front=(((y-yt>=1)&&(y-yt<=line)&&(x-xt>=1)&&(x-xt<=row))&&
-						(((pb[ARRY(x-xt-1,y-yt)].front!=4)&&(pb[ARRY(x-xt-1,y-yt)].back==0))
-						||((pb[ARRY(x-xt+1,y-yt)].front!=4)&&(pb[ARRY(x-xt+1,y-yt)].back==0))
-						||((pb[ARRY(x-xt,y-yt-1)].front!=4)&&(pb[ARRY(x-xt,y-yt-1)].back==0))
-						||((pb[ARRY(x-xt,y-yt+1)].front!=4)&&(pb[ARRY(x-xt,y-yt+1)].back==0))))?
-						0
-						:(pb[ARRY(x-xt,y-yt)].front);
-		})
+			({
+			 int tier;
+			 int xt,yt;
+			 pb[ARRY(x,y)].front=0;
+			 for(tier=1;({
+						 int canOpenBlock=0;
+						 for(yt=tier;yt>(-tier-1)&&canOpenBlock==0;yt--)
+						 for(xt=tier;xt>(-tier-1)&&canOpenBlock==0;xt-=((yt==tier)||(yt==(-tier)))?1:(tier*2))
+						 canOpenBlock=(((y-yt>=1)&&(y-yt<=line)&&(x-xt>=1)&&(x-xt<=row))&&
+								 (((pb[ARRY(x-xt-1,y-yt)].front==1)&&(pb[ARRY(x-xt-1,y-yt)].back==0))
+								  ||((pb[ARRY(x-xt+1,y-yt)].front==1)&&(pb[ARRY(x-xt+1,y-yt)].back==0))
+								  ||((pb[ARRY(x-xt,y-yt-1)].front==1)&&(pb[ARRY(x-xt,y-yt-1)].back==0))
+								  ||((pb[ARRY(x-xt,y-yt+1)].front==1)&&(pb[ARRY(x-xt,y-yt+1)].back==0)))
+								 &&((pb[ARRY(x-xt,y-yt)].front==1)&&(pb[ARRY(x-xt,y-yt)].back!=9)))?
+						 1
+						 :0;
+						 canOpenBlock;})
+					 ;tier+=(
+						 {
+						 int haveUnopenedBlock=0;
+						 for(yt=tier;yt>(-tier-1)&&haveUnopenedBlock==0;yt--)
+						 for(xt=tier;xt>(-tier-1)&&haveUnopenedBlock==0;xt-=((yt==tier)||(yt==(-tier)))?1:(tier*2))
+						 haveUnopenedBlock=(((y-yt>=1)&&(y-yt<=line)&&(x-xt>=1)&&(x-xt<=row))&&
+								 (((pb[ARRY(x-xt-1,y-yt)].front!=4)&&(pb[ARRY(x-xt-1,y-yt)].back==0))
+								  ||((pb[ARRY(x-xt+1,y-yt)].front!=4)&&(pb[ARRY(x-xt+1,y-yt)].back==0))
+								  ||((pb[ARRY(x-xt,y-yt-1)].front!=4)&&(pb[ARRY(x-xt,y-yt-1)].back==0))
+								  ||((pb[ARRY(x-xt,y-yt+1)].front!=4)&&(pb[ARRY(x-xt,y-yt+1)].back==0)))
+								 &&((pb[ARRY(x-xt,y-yt)].front==1)&&(pb[ARRY(x-xt,y-yt)].back!=9)))?
+						 1
+						 :0;
+						 !haveUnopenedBlock;
+						 }))
+			 for(yt=tier;yt>(-tier-1);yt--)
+				 for(xt=tier;xt>(-tier-1);xt-=((yt==tier)||(yt==(-tier)))?1:(tier*2))
+					 pb[ARRY(x-xt,y-yt)].front=(((y-yt>=1)&&(y-yt<=line)&&(x-xt>=1)&&(x-xt<=row))&&
+							 (((pb[ARRY(x-xt-1,y-yt)].front!=4)&&(pb[ARRY(x-xt-1,y-yt)].back==0))
+							  ||((pb[ARRY(x-xt+1,y-yt)].front!=4)&&(pb[ARRY(x-xt+1,y-yt)].back==0))
+							  ||((pb[ARRY(x-xt,y-yt-1)].front!=4)&&(pb[ARRY(x-xt,y-yt-1)].back==0))
+							  ||((pb[ARRY(x-xt,y-yt+1)].front!=4)&&(pb[ARRY(x-xt,y-yt+1)].back==0))))?
+						 0
+						 :(pb[ARRY(x-xt,y-yt)].front);
+			})
 		:((pb[ARRY(x,y)].back==9)?(gameOver(0)):(pb[ARRY(x,y)].front=0));
 	}
 	if(ifGameOver==0)print();
